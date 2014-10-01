725a0a29f3b34d3e54d2f43b7825626d2280fe58
mashijp <mashijp@gmail.com>
2014-10-02 01:09:31 +0900

    Determinate image type (GIF, PNG)

--- main.c
+++ main.c
@@ -3,10 +3,17 @@
 #include <gd.h>
 #include <getopt.h>
 #include <stdlib.h>
+#include <Kernel/string.h>
+
+#define EXIT_FAILURE_COMMON 1
+#define EXIT_CANNOT_OPEN_FILE 2
+#define FILE_HEAD_READ_SIZE 8
 
 void usage();
 
-enum gdcmdImageType {GIF, JPEG, PNG, UNKNOWN};
+enum gdcmdImageType {
+    GIF, JPEG, PNG, UNKNOWN
+};
 
 char *filename;
 
@@ -37,10 +44,46 @@ void option(int argc, char *argv[]) {
     filename = argv[optind];
 }
 
+int headcmp(char c1[], char c2[], int size) {
+    int result = 1;
+    for (int i = 0; i < size; i++) {
+        if (c1[i] != c2[i]) {
+            result = 0;
+            break;
+        }
+    }
+    return result;
+}
+
 int main(int argc, char *argv[]) {
     option(argc, argv);
     FILE *fp = fopen(filename, "rb");
-    if (fp == NULL) fprintf(stderr, "Cannot open the file: %s\n", filename);
+    if (fp == NULL) {
+        fprintf(stderr, "Cannot open the file: %s\n", filename);
+        exit(EXIT_CANNOT_OPEN_FILE);
+    }
+    char head[FILE_HEAD_READ_SIZE];
+    size_t result = fread(head, 1, FILE_HEAD_READ_SIZE, fp);
+    if (result < FILE_HEAD_READ_SIZE) {
+        fprintf(stderr, "The file may not be an image file");
+        exit(EXIT_FAILURE_COMMON);
+    }
+
+    char gif_head[] = {'G', 'I', 'F'};
+    int gif_head_size = 3;
+
+    char png_head[] = {0x89, 0x50, 0x4e, 0x47, 0x0D, 0x0A, 0x1A, 0x0A};
+    int png_head_size = 8;
+
+    if (headcmp(head, gif_head, gif_head_size)) {
+        fprintf(stdout, "GIF");
+    } else if (headcmp(head, png_head, png_head_size)){
+        fprintf(stdout, "PNG");
+    } else {
+        fprintf(stderr, "Unknown image type");
+        exit(EXIT_FAILURE_COMMON);
+    }
+
     fclose(fp);
 
     return 0;
@@ -49,7 +92,7 @@ int main(int argc, char *argv[]) {
 void usage() {
     fprintf(stderr, "GDCmd Version %s (libgd %s)\n", GDCMD_VERSION_STRING, GD_VERSION_STRING);
     fprintf(stderr, "%s\n%s\n",
-    "gdidentify [filename]",
-    "ex) gdidentify /tmp/fuga.gif");
-    exit(1);
+            "gdidentify [filename]",
+            "ex) gdidentify /tmp/fuga.gif");
+    exit(EXIT_FAILURE_COMMON);
 }
